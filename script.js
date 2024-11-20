f04ddbc53dfd7bd3bdbb2598079ff5cb389cb8f5
Amrito Dey <amritodey07@gmail.com>
2024-11-20 15:44:35 +0600

    first page complete

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // Initialize AOS
 AOS.init({
-    duration: 1000, // Animation duration in milliseconds
-    once: true, // Whether animation should happen only once - while scrolling down
+  duration: 1000, // Animation duration in milliseconds
+  once: true, // Whether animation should happen only once - while scrolling down
 });
 
-// Optional: Additional custom JavaScript can be added here
+// function sidePanelOpen() {
+//   const sidePanel = document.createElement("div");
+//   sidePanel.innerHTML = ``;
+// const panelOpen = document.getElementById("panel-element");
+// panelOpen.append(sidePanel);
+// }
