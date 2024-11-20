53912340ee608bee1a42c569f04f165d7294066e
Amrito Dey <amritodey07@gmail.com>
2024-11-20 14:22:17 +0600

    main structure complete

---
+++ script.js
@@ -0,0 +1,7 @@
+// Initialize AOS
+AOS.init({
+    duration: 1000, // Animation duration in milliseconds
+    once: true, // Whether animation should happen only once - while scrolling down
+});
+
+// Optional: Additional custom JavaScript can be added here
